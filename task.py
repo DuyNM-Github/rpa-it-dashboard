ddfce0007c48cd265c542c19cad537e231a31a5f
thao <thaodd@vissoft.vn>
2021-11-11 15:06:05 +0700

    fix issues

--- task.py
+++ task.py
@@ -154,9 +154,9 @@ def download_pdfs():
 
 
 def move_pdfs():
-    source = f"{os.getcwd()}/" + file + ".pdf"
     destination = output_folder + file + ".pdf"
     for file in list_of_links.keys():
+        source = f"{os.getcwd()}/" + file + ".pdf"
         while filesys.does_file_not_exist(destination) is True:
             try:
                 shutil.move(source, destination)
